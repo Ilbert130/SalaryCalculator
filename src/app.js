1888bee570fb244f34a47cabb810c89757589eb4
Ilbert130 <ilbertca27@gmail.com>
2023-04-02 13:34:03 -0400

    getting the calculation result and render the table with the result

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-
+import { christmasBonus } from "./uses-cases/christmas-bonus";
+import { notice } from "./uses-cases/notice";
+import { renderTable } from "./uses-cases/render-table";
+import { unemployment } from "./uses-cases/unemployment";
+import { vacation } from "./uses-cases/vacation";
 
 //This is the main method
 export const app = (elementClass) => {
@@ -6,6 +10,7 @@ export const app = (elementClass) => {
     //references to the DOM
     const conteiner = document.querySelector(elementClass);
     const formResult = document.querySelector('.myForm');
+    let noticeRes, unemploymentRes, christmasBonusRes, vacationRes;
 
     //Event to get data from the form
     formResult.addEventListener('submit', e => {
@@ -16,5 +21,18 @@ export const app = (elementClass) => {
             new FormData(e.target)
         );
 
+        const dailySalary = (kindSalary === 'Monthly') ? Math.round(salary/23.83) : Math.round(salary/11.91);
+
+        christmasBonusRes = christmasBonus((kindSalary === 'Monthly') ? salary*1 : salary*2, actualYearMonths * 1);
+
+        if((!year || year === '0') && month * 1 < 3){
+            conteiner.innerHTML = renderTable(christmasBonusRes);
+        }
+        
+        noticeRes = notice(dailySalary, year * 1, month * 1);
+        unemploymentRes = unemployment(dailySalary, year * 1, month * 1);
+        vacationRes= vacation(dailySalary, year * 1)
+
+        conteiner.innerHTML = renderTable(christmasBonusRes, noticeRes, unemploymentRes, vacationRes);
     });
 }
