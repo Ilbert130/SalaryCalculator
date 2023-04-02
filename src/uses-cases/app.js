ffcaf010096b004945fc14961dc6d23f31f7b826
Ilbert130 <ilbertca27@gmail.com>
2023-04-02 13:15:57 -0400

    Creating the references to the DOM

--- src/uses-cases/app.js
+++ src/uses-cases/app.js
@@ -3,5 +3,7 @@
 //This is the main method
 export const app = (elementClass) => {
 
-
+    //references to the DOM
+    const conteiner = document.querySelector(elementClass);
+    const formResult = document.querySelector('.myForm');
 }
