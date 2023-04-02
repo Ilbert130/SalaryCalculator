e88d5685cb589f0d68cd117e3bb18310c633a705
Ilbert130 <ilbertca27@gmail.com>
2023-04-02 13:18:34 -0400

    Creating the event to get the data from the form

--- src/uses-cases/app.js
+++ src/uses-cases/app.js
@@ -6,4 +6,15 @@ export const app = (elementClass) => {
     //references to the DOM
     const conteiner = document.querySelector(elementClass);
     const formResult = document.querySelector('.myForm');
+
+    //Event to get data from the form
+    formResult.addEventListener('submit', e => {
+        e.preventDefault();
+
+        //Getting the data from the form
+        const {salary, year, actualYearMonths, month, kindSalary} = Object.fromEntries(
+            new FormData(e.target)
+        );
+
+    });
 }
