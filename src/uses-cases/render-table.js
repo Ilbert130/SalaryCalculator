8b9f9c3e26883c16a7b21e592db3aff9bf00e0fd
Ilbert130 <ilbertca27@gmail.com>
2023-04-02 13:30:35 -0400

    Creating the method to render the table with the result

--- src/uses-cases/render-table.js
+++ src/uses-cases/render-table.js
@@ -1,11 +1,40 @@
 
 
+//Creating the method to render the table with the result
+export const renderTable = (christmasBonus = 0, notice = 0, unemployment = 0, vacation = 0) => {
 
-
-
-
-
-
+    const total =notice+unemployment+christmasBonus+vacation;
+  
+    return `<table class="table table-bordered">
+      <thead>
+        <tr>
+          <th colspan="2" class="center">Result</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">Notice</th>
+          <td>${currencyFormatter({currency: 'USD', value:notice})}</td>
+        </tr>
+        <tr>
+          <th scope="row">Unemployment</th>
+          <td>${currencyFormatter({currency: 'USD', value:unemployment})}</td>
+        </tr>
+        <tr>
+          <th scope="row">Christmas Bonus</th>
+          <td>${currencyFormatter({currency: 'USD', value:christmasBonus})}</td>
+        </tr>
+        <tr>
+          <th scope="row">Vacation</th>
+          <td>${currencyFormatter({currency: 'USD', value:vacation})}</td>
+        </tr>
+        <tr>
+          <th scope="row">Total</th>
+          <td>${currencyFormatter({currency: 'USD', value:total})}</td>
+        </tr>
+      </tbody>
+    </table>`;
+}
 
 //Creating money format
 const currencyFormatter = ({ currency, value}) => {
